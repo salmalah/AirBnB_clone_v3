9e0ded840758e796e7f6c9d74d6374f5c42502b3
salmalah <122815714+salmalah@users.noreply.github.com>
2023-10-28 18:03:04 +0100

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ Flask Application """
 from models import storage
-from api.v1.views import app_views
-from os import environ
-from flask import Flask, jsonify
 from flask_cors import CORS
+from os import environ
+from api.v1.views import app_views
+from flask import Flask, jsonify
 
 
 app = Flask(__name__)
@@ -13,26 +13,23 @@ CORS(app, origins="0.0.0.0")
 
 
 @app.teardown_appcontext
-def stor_close(err):
-    """ calls storage close """
+def close_storage(error):
+    """ Closes the database connection  """
     storage.close()
 
 
 @app.errorhandler(404)
-def notfound_err(err):
+def not_found_error(error):
     """
-    returns a JSON-formatted 404 status code response
-    content : "error": "Not found"
+    Handles 404 errors by returning a JSON-formatted response
+    content : {"error": "Not found"}
     """
     return jsonify(error="Not found"), 404
 
 
 if __name__ == "__main__":
-    """ Run the Flask server (variable app) """
-    host = environ.get('HBNB_API_HOST')
-    if not host:
-        host = '0.0.0.0'
-    port = environ.get('HBNB_API_PORT')
-    if not port:
-        port = '5000'
+    """ Run the Flask server """
+    app = create_app()
+    host = environ.get('HBNB_API_HOST', '0.0.0.0')
+    port = int(environ.get('HBNB_API_PORT', 5000))
     app.run(host=host, port=port, threaded=True)
